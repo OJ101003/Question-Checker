4d2ffbbb2174b83d06dc35aaa5557eb81fd4ef7e
OJ101003 <73129639+OJ101003@users.noreply.github.com>
2022-09-25 21:50:19 -0700

    Update QuestionChecker.py

--- QuestionChecker.py
+++ QuestionChecker.py
@@ -4,7 +4,7 @@ def questionChecker(userInput):
 #Gets user input and stores it in a variable
 userInput = input("Put in the sentence.\n")
 # Uses the function to find if there is a question mark in the user inputted sentence and prints a statement accordingly
-if questionChecker(userInput) == True:
+if questionChecker(userInput):
     print("Statement is a question.")
 else:
     print("Statement is not a question.")
