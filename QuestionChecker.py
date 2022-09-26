b1fdf3a9daba03f01ec022a095db23a5a52190b2
OJ101003 <73129639+OJ101003@users.noreply.github.com>
2022-09-25 21:49:25 -0700

    Add files via upload

---
+++ QuestionChecker.py
@@ -0,0 +1,10 @@
+# Function to return a boolean that is true if a question mark is found in the userInput variable
+def questionChecker(userInput):
+    return "?" in userInput
+#Gets user input and stores it in a variable
+userInput = input("Put in the sentence.\n")
+# Uses the function to find if there is a question mark in the user inputted sentence and prints a statement accordingly
+if questionChecker(userInput) == True:
+    print("Statement is a question.")
+else:
+    print("Statement is not a question.")
